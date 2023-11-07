91095d984d8bf35c51a99e728f31ed70a8b84038
Artemi Lapitski <artsemilapitski@gmail.com>
2023-11-07 13:42:01 +0100

    Deleted the file from the git repository

--- testik.py
+++
@@ -1,7 +0,0 @@
-from app.database import connect_db
-from app import courses, COURSES, add_courses
-
-
-session = connect_db()
-
-add_courses(session, COURSES, courses)
--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-from pytest import fixture
-from app import database, get_session
-
-@fixture(autouse=True)
-def mock_database(mocker):
-    mocker.patch.object(database, 'DATABASE_URL', 'sqlite:///:memory:')
-    test_db = database.connect_db()
-    test_db.create_tables(MODELS)
-    return test_db
