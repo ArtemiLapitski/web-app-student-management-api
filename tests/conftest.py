398d1a239424e27ce67e6e89bd37d93c000a1c36
ArtemiLapitski <artsemilapitski@gmail.com>
2024-01-23 17:15:05 +0100

    23.01.2024

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,127 +1,99 @@
-# from sqlalchemy import URL, create_engine, text
-# from sqlalchemy.orm import Session
-# from config import (DB_USERNAME, DB_PASSWORD, DB_ROLE, DB_NAME, DB_HOST, DB_SUPERUSER_PASSWORD,
-#                     DB_SUPERUSER_USERNAME, DB_PORT, CREATE_TABLES_SQL_FILE_PATH)
-# from app.database.setup import (create_db, create_tables, add_data, get_session)
-# from main import create_app, create_api
-# from app.urls import add_urls
-# from tests.mocks import groups_mock, courses_mock, data_by_student_mock
-# import pytest
-#
-#
-#
-# @pytest.fixture(scope='session')
-# def db_setup(request):
-#     db_engine = create_db(DB_SUPERUSER_USERNAME,
-#                        DB_SUPERUSER_PASSWORD,
-#                        DB_USERNAME,
-#                        DB_PASSWORD,
-#                        DB_NAME,
-#                        DB_ROLE,
-#                        DB_PORT,
-#                        DB_HOST)
-#
-#     create_tables(db_engine, CREATE_TABLES_SQL_FILE_PATH)
-#     db_session = get_session(db_engine)
-#     with db_session:
-#         add_data(db_session, groups=groups_mock, courses=courses_mock, data_by_student=data_by_student_mock)
-#
-#     def teardown():
-#         superuser_url = URL.create(
-#             "postgresql",
-#             username=DB_SUPERUSER_USERNAME,
-#             password=DB_SUPERUSER_PASSWORD,
-#             host=DB_HOST,
-#             port=DB_PORT
-#         )
-#         engine = create_engine(superuser_url)
-#         with engine.connect() as conn:
-#             conn.execution_options(isolation_level="AUTOCOMMIT")
-#             conn.execute(text(f"DROP DATABASE {DB_NAME} WITH (FORCE)"))
-#             conn.execute(text(f"DROP USER {DB_USERNAME}"))
-#             conn.execute(text(f"DROP ROLE {DB_ROLE}"))
-#
-#     request.addfinalizer(teardown)
-#
-#     return db_engine
-#
-#     # yield db_engine
-#     # drop_database(f'postgresql://{"postgres"}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
-#
-#
-# @pytest.fixture(scope='function')
-# def db_session(db_setup):
-#     """Returns an sqlalchemy session, and after the test tears down everything properly."""
-#     connection = db_setup.connect()
-#     # begin the nested transaction
-#     transaction = connection.begin()
-#     # use the connection with the already started transaction
-#     session = Session(bind=connection)
-#
-#     yield session
-#     session.rollback()
-#     connection.close()
-
-
-
-
 import pytest
-from sqlalchemy import URL, create_engine, text
-from sqlalchemy.orm import sessionmaker
-
-from app.database.models import Base
-from config import (DB_USERNAME, DB_PASSWORD, DB_ROLE, DB_NAME, DB_HOST, DB_SUPERUSER_PASSWORD,
-                    DB_SUPERUSER_USERNAME, DB_PORT, CREATE_TABLES_SQL_FILE_PATH)
-from pytest import fixture
-from app.database.setup import (create_db, create_tables, add_data, get_session)
 from main import create_app, create_api
 from app.urls import add_urls
 from tests.mocks import groups_mock, courses_mock, data_by_student_mock
-from sqlalchemy_utils import database_exists, create_database, drop_database
-
-from sqlalchemy.orm import Session
+from config import DB_URL
+from sqlalchemy import create_engine, URL, text
+from app.database.setup import create_db_and_user, create_tables, get_session, add_data
+from config import (DB_USERNAME, DB_PASSWORD, DB_ROLE, DB_NAME, DB_HOST, DB_SUPERUSER_PASSWORD,
+                    DB_SUPERUSER_USERNAME, DB_PORT, CREATE_TABLES_SQL_FILE_PATH)
+from app.database.models import db
 
 
 @pytest.fixture(scope="session")
-def db_engine():
-    TEST_DB_URL = f'postgresql://{"postgres"}:{DB_SUPERUSER_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+def db_setup(request):
+    create_db_and_user(superuser_username=DB_SUPERUSER_USERNAME,
+                       superuser_password=DB_SUPERUSER_PASSWORD,
+                       username=DB_USERNAME,
+                       password=DB_PASSWORD,
+                       db_name=DB_NAME,
+                       role=DB_ROLE,
+                       port=DB_PORT,
+                       host=DB_HOST)
 
-    engine = create_engine(TEST_DB_URL)
-    if not database_exists(TEST_DB_URL):
-        create_database(engine.url)
-    Base.metadata.create_all(bind=engine)
-    db_session = get_session(engine)
-    with db_session:
-        add_data(db_session, groups=groups_mock, courses=courses_mock, data_by_student=data_by_student_mock)
+    engine = create_engine(DB_URL)
 
-    yield engine
-    drop_database(TEST_DB_URL)
+    create_tables(engine, CREATE_TABLES_SQL_FILE_PATH)
+
+    session = get_session(engine)
+
+    with session:
+        add_data(session, groups=groups_mock, courses=courses_mock, data_by_student=data_by_student_mock)
+
+    def teardown():
+        superuser_url = URL.create(
+            "postgresql",
+            username=DB_SUPERUSER_USERNAME,
+            password=DB_SUPERUSER_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        engine = create_engine(superuser_url)
+        with engine.connect() as conn:
+            conn.execution_options(isolation_level="AUTOCOMMIT")
+            conn.execute(text(f"DROP DATABASE {DB_NAME} WITH (FORCE)"))
+            conn.execute(text(f"DROP USER {DB_USERNAME}"))
+            conn.execute(text(f"DROP ROLE {DB_ROLE}"))
+
+    request.addfinalizer(teardown)
 
 
-@pytest.fixture(scope="function")
-def db(db_engine):
-    connection = db_engine.connect()
-    transaction = connection.begin()
-    session = Session(bind=connection)
-    yield session
-    session.rollback()
-    connection.close()
-
-
-
-
-
-
-
-
-@pytest.fixture()
-def app():
+@pytest.fixture(scope="session")
+def client(request):
     app = create_app()
-    return app
-
-
-@pytest.fixture()
-def client(app):
+    app.config['TESTING'] = True
+    db.init_app(app)
     api = create_api(app)
     add_urls(api)
-    return app.test_client()
+    client = app.test_client()
+
+    ctx = app.app_context()
+    ctx.push()
+
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
+    return client
+
+
+# @pytest.fixture(scope="session")
+# def db():
+#     return db
+
+
+@pytest.fixture(scope='function')
+def session(client, request):
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    options = dict(bind=connection, binds={})
+    session = db.create_scoped_session(options=options)
+
+    db.session = session
+
+    def teardown():
+        session.close()
+        transaction.rollback()
+        connection.close()
+        session.remove()
+
+    request.addfinalizer(teardown)
+    return session
+
+
+# @pytest.fixture(scope="session")
+# def sqlalchemy_session():
+#     engine = create_engine(DB_URL)
+#     session = get_session(engine)
+#     return session
